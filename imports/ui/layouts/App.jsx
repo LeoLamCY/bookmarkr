1bd159e521ef4dea9d64780d8053608205594450
Leo Lam <leolam.cy@gmail.com>
2016-05-03 03:47:36 -0400

    add main page elements

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
- 
+
 // App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header>
-          <h2>Bookmark Tags</h2>
-        </header>
+      <div className="ui container grid">
+        <div className="ui left fixed vertical inverted menu">
+          <div className="item">
+            <a href="/"><img className="ui small image" src="img/logo.png" /></a>
+          </div>
+          <div className="item">
+              <h4 className="ui inverted header">Tags</h4>
+              <div className="menu">
+                <a href="#" className="item">Tag1</a>
+              </div>
+          </div>
+        </div>
+        <div className="ui main grid">
+            <select className="ui fluid search dropdown" multiple="">
+
+            </select>
+        </div>
       </div>
     );
   }
