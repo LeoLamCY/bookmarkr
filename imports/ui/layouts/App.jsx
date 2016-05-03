a1d61d0e89bc94bcf4e5f0dad9a9d09d631092e8
Leo Lam <leolam.cy@gmail.com>
2016-05-03 00:02:01 -0400

    init commit

---
+++ imports/ui/layouts/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h2>Bookmark Tags</h2>
+        </header>
+      </div>
+    );
+  }
+}
